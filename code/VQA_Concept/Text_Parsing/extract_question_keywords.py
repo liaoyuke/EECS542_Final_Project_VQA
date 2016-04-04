c6e0caf41f854fd3f1b025fab4ffd9cc8a328616
Yuke Liao <liaoyuke@umich.edu>
2016-04-03 22:52:20 -0400

    Added script to parse questions given input file and output to a file

---
+++ code/VQA_Concept/Text_Parsing/extract_question_keywords.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import sys
+from get_keywords import *
+
+input_file_path = sys.argv[1]
+output_file_path = sys.argv[2]
+
+with open(input_file_path) as fin:
+    questions = fin.readlines()
+
+answer_strs = []
+for question in questions:
+    answer = get_keywords(question)
+    answer_str = '-'.join(answer)
+    print answer_str
+    answer_strs.append(answer_str)
+
+fout = open(output_file_path, 'w')
+for answer_str in answer_strs:
+    fout.write(answer_str + '\n')
+
+fout.close()
--- code/VQA_Concept/Text_Parsing/get_keywords.py
+++ code/VQA_Concept/Text_Parsing/get_keywords.py
@@ -5,8 +5,10 @@ from cortical.client import ApiClient
 from cortical.termsApi import TermsApi
 from cortical.textApi import TextApi
 
+client = ApiClient(apiKey="5eeadef0-f9f6-11e5-8378-4dad29be0fab", apiServer="http://api.cortical.io/rest")
+api = TextApi(client)
+
 def get_keywords(sentence):
-	client = ApiClient(apiKey="5eeadef0-f9f6-11e5-8378-4dad29be0fab", apiServer="http://api.cortical.io/rest")
-	api = TextApi(client)
 	keywordList = api.getKeywordsForText("en_associative", sentence)
-	print(keywordList)
+	return keywordList
+	# print(keywordList)
