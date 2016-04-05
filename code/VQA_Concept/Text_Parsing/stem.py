8f758a76f2190c6bdd351d7434d79915d0f67b09
Yuke Liao <liaoyuke@umich.edu>
2016-04-04 21:35:08 -0400

    Fixed bugs in stem.py and rerun to stem keywords extracted from questions.

--- code/VQA_Concept/Text_Parsing/stem.py
+++ code/VQA_Concept/Text_Parsing/stem.py
@@ -4,6 +4,9 @@ import en
 
 def stem(word):
 	result = en.verb.infinitive(word)
-	if len(result) is 0:
-		result = en.noun.singular(word)
-	return result
+	if len(result) != 0 and en.is_verb(result):
+		return result
+	result = en.noun.singular(word)
+	if len(result) != 0 and en.is_noun(result):
+		return result
+	return word
