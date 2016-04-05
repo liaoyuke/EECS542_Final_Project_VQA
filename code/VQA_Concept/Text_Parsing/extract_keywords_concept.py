7491a827b389b392111b087832d943331d8c6453
Yuke Liao <liaoyuke@umich.edu>
2016-04-05 12:01:16 -0400

    Created file to extract stemmed keywords and extract concept along with simple test cases.

---
+++ code/VQA_Concept/Text_Parsing/extract_keywords_concept.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import sys
+import os
+import json
+from get_commonsense import *
+
+input_file_path = sys.argv[1]
+output_file_path = sys.argv[2]
+
+concept_map = {}
+if os.path.isfile(output_file_path):
+	with open(output_file_path) as concept_map_file:
+		concept_map = json.load(concept_map_file)
+
+with open(input_file_path) as fin:
+	list_words = fin.readlines()
+
+for word in list_words:
+	word = word.rstrip()
+	if word in concept_map:
+		continue
+	list_concepts = get_commonsense(word.replace(' ', '_'), 50)
+	concept_map[word] = list_concepts
+
+with open(output_file_path, 'w') as outfile:
+	json.dump(concept_map, outfile)
---
+++ code/VQA_Concept/Text_Parsing/extract_unique_keywords.py
@@ -0,0 +1,22 @@
+input_files = []
+input_files.append("questions/coco_trainval2014_question_keywords_stem.txt")
+input_files.append("questions/coco_trainval2014_train_question_keywords_stem.txt")
+input_files.append("questions/coco_trainval2014_val_question_keywords_stem.txt")
+
+output_file = "questions/unique_keywords.txt"
+
+unique_keywords = set()
+for input_file in input_files:
+    with open(input_file) as fin:
+        list_str_words = fin.readlines()
+        for str_words in list_str_words:
+            str_words = str_words.rstrip()
+            words = str_words.split('-')
+            for word in words:
+                if len(word) > 0:
+                    unique_keywords.add(word)
+
+fout = open(output_file, 'w')
+for unique_keyword in unique_keywords:
+    fout.write(unique_keyword + '\n')
+fout.close()
