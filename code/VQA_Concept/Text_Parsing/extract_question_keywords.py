df141781bb499986e84553a4c311894c38b50e73
Yuke Liao <liaoyuke@umich.edu>
2016-04-07 01:22:15 -0400

    Found some issues with the extracted keywords, set up environments to rerun the extraction process.

--- code/VQA_Concept/Text_Parsing/extract_question_keywords.py
+++ code/VQA_Concept/Text_Parsing/extract_question_keywords.py
@@ -7,9 +7,12 @@ output_file_path = sys.argv[2]
 
 with open(input_file_path) as fin:
     questions = fin.readlines()
-
+i = 1
 answer_strs = []
 for question in questions:
+    print i
+    i = i + 1
+    question = question.rstrip()
     answer = get_keywords(question)
     answer_str = '-'.join(answer)
     print answer_str
--- code/VQA_Concept/Text_Parsing/get_keywords_test.py
+++ code/VQA_Concept/Text_Parsing/get_keywords_test.py
@@ -3,4 +3,4 @@ from get_keywords import *
 sentence  = "placeholder"
 while sentence != "exit":
 	sentence = raw_input("Enter the Sentence: ")
-	get_keywords(sentence)
+	print get_keywords(sentence)
